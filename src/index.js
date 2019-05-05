09b1dc494d86b4fe48d2abf386766e2ea3de4b57
Katie George <katiegeorge94@gmail.com>
2019-05-05 13:02:09 -0700

    loop for routes

--- src/index.js
+++ src/index.js
@@ -11,22 +11,46 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fabLinkedIn } from '@fortawesome/free-solid-svg-icons'
 
+const tabs = [
+    {
+        label: 'About',
+        id: 1,
+        path: '/about',
+    },
+    {
+        label: 'Design',
+        id: 2,
+        path: '/design',
+    },
+    {
+        label: 'Code',
+        id: 3,
+        path: '/code',
+    },
+    {
+        label: 'Contact',
+        id: 4,
+        path: '/contact',
+    },
+];
+
 const routing = (
   <Router>
     <div className="app">
       <ul className="nav">
-        <li className="nav--list">
-          <NavLink className="nav--item nav--item__1" activeClassName="nav--item__1__active" to="/about">About</NavLink>
-        </li>
-        <li className="nav--list">
-          <NavLink className="nav--item nav--item__2" activeClassName="nav--item__2__active" to="/code">Code</NavLink>
-        </li>
-        <li className="nav--list">
-          <NavLink className="nav--item nav--item__3" activeClassName="nav--item__3__active" to="/design">Design</NavLink>
-        </li>
-        <li className="nav--list">
-          <NavLink className="nav--item nav--item__4" activeClassName="nav--item__4__active" to="/contact">Contact</NavLink>
-        </li>
+        {
+            tabs.map(tab => (
+                <li className="nav--list">
+                    <NavLink
+                        className={ `nav--item nav--item__${tab.id}` }
+                        activeClassName={ `nav--item__${tab.id}__active` }
+                        to={ tab.path }
+                    >
+                        { tab.label }
+                    </NavLink>
+                </li>
+            ))
+        }
       </ul>
       <Route exact path="/about" component={About} />
       <Route path="/design" component={Design} />
