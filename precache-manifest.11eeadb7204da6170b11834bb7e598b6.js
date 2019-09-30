1684e7b628ff6c7dd35a885d32e02f6a1b717cbe
Katie George <katiegeorge94@gmail.com>
2019-09-30 10:15:49 -0700

    Updates

---
+++ precache-manifest.11eeadb7204da6170b11834bb7e598b6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ffbc33b2646d01961bd19e044d79277",
+    "url": "/portfolio/static/media/mobile1.0ffbc33b.png"
+  },
+  {
+    "revision": "b3460052dc5864d84a9c",
+    "url": "/portfolio/static/css/main.f7773e71.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "a69e3d4b590aa947f322",
+    "url": "/portfolio/static/js/2.37e35776.chunk.js"
+  },
+  {
+    "revision": "b45ffa3f736ae4cfd47f75976ed6a9be",
+    "url": "/portfolio/static/media/headshot.b45ffa3f.png"
+  },
+  {
+    "revision": "dcb76a2734f18e30d2d7539447cd37bf",
+    "url": "/portfolio/static/media/kingsofleon.dcb76a27.jpg"
+  },
+  {
+    "revision": "2ff55d4d185f522499442867a0a8afc2",
+    "url": "/portfolio/static/media/youngthegiant.2ff55d4d.jpg"
+  },
+  {
+    "revision": "fd066832e0068f279e30a1d760461a60",
+    "url": "/portfolio/static/media/howibuiltthis.fd066832.jpg"
+  },
+  {
+    "revision": "bf46d0a228c0b2e21ff93b280e54fa06",
+    "url": "/portfolio/static/media/hiddenbrain.bf46d0a2.jpg"
+  },
+  {
+    "revision": "116ac139e0a0e00aed857b237d53a647",
+    "url": "/portfolio/static/media/freakonomics.116ac139.jpg"
+  },
+  {
+    "revision": "853e0c41852d09b40fe1dff203e3e570",
+    "url": "/portfolio/static/media/lakestreetdive.853e0c41.jpg"
+  },
+  {
+    "revision": "1a9b2a76e458ca10a9f4894fcd49d560",
+    "url": "/portfolio/static/media/Bell-color.1a9b2a76.png"
+  },
+  {
+    "revision": "1881e50bd00fc081d825365639f3d566",
+    "url": "/portfolio/static/media/VR-color.1881e50b.png"
+  },
+  {
+    "revision": "06badd2e9b0b6fe4a4de96daceafd7b5",
+    "url": "/portfolio/static/media/vr-timeline-1.06badd2e.png"
+  },
+  {
+    "revision": "dce65330e6a93fb80600d2f48fe92fe8",
+    "url": "/portfolio/static/media/vr-timeline-2.dce65330.png"
+  },
+  {
+    "revision": "b3460052dc5864d84a9c",
+    "url": "/portfolio/static/js/main.e8d8fb77.chunk.js"
+  },
+  {
+    "revision": "c0692bdffb7549c287b95146970c18c5",
+    "url": "/portfolio/static/media/mobile2.c0692bdf.png"
+  },
+  {
+    "revision": "78d121c4b60aba1c13c47fb0cb94f084",
+    "url": "/portfolio/static/media/mobile3.78d121c4.png"
+  },
+  {
+    "revision": "8b8ac3e3ce1f579b57ab02fd5ccf2152",
+    "url": "/portfolio/static/media/mobile4.8b8ac3e3.png"
+  },
+  {
+    "revision": "eab00e7b19c8b85783e32d6b31a0a166",
+    "url": "/portfolio/static/media/alert1.eab00e7b.png"
+  },
+  {
+    "revision": "2b006a85ba154fafe563aae0e23a873c",
+    "url": "/portfolio/static/media/alert2.2b006a85.png"
+  },
+  {
+    "revision": "8780182325ae5252073e02c6ecb16765",
+    "url": "/portfolio/static/media/Compass-color.87801823.png"
+  },
+  {
+    "revision": "7ab2ddc2404d8f0e6e44922e9c8e7874",
+    "url": "/portfolio/static/media/Investment-color.7ab2ddc2.png"
+  },
+  {
+    "revision": "72c511dd29b7ddaea3a859fa9c0befad",
+    "url": "/portfolio/static/media/Octopus-color.72c511dd.png"
+  },
+  {
+    "revision": "5a7b74806031f504b36f45b0291d30a1",
+    "url": "/portfolio/static/media/sensorysea1.5a7b7480.png"
+  },
+  {
+    "revision": "4f9c6e7d31551890d8f1dec479012ddf",
+    "url": "/portfolio/static/media/sensorysea2.4f9c6e7d.png"
+  },
+  {
+    "revision": "4b43a34d626b3798ede87d316f93148a",
+    "url": "/portfolio/static/media/investorlandingpage1.4b43a34d.png"
+  },
+  {
+    "revision": "812cd5628b6373b766a4ba789d8c90b7",
+    "url": "/portfolio/static/media/investorlandingpage3.812cd562.png"
+  },
+  {
+    "revision": "b49486e2f79118ee68957f0d69c1bb40",
+    "url": "/portfolio/static/media/nav1.b49486e2.png"
+  },
+  {
+    "revision": "8397cb2a5db95f8b557faf68a96582d2",
+    "url": "/portfolio/static/media/Logo.8397cb2a.png"
+  },
+  {
+    "revision": "9399172ed8f64146a87bbac1938f2867",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c0af40bb4e2ffb9e2ce40a71b380daf.js"
+  "/portfolio/precache-manifest.11eeadb7204da6170b11834bb7e598b6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
